ac37363260a5d48e3e1df223e96a874f0c7a6a4c
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-08-03 18:15:25 +0300

    fixed. last line added

--- homework/dima_novolodskiy/Homework_2/hw_2_1.py
+++ homework/dima_novolodskiy/Homework_2/hw_2_1.py
@@ -4,3 +4,4 @@ sum_ab = a + b
 raz_ab = a - b
 pro_ab = a * b
 print(f'Сумма: {sum_ab} Разность: {raz_ab} Произведение: {pro_ab}')
+
--- homework/dima_novolodskiy/Homework_2/hw_2_2.py
+++ homework/dima_novolodskiy/Homework_2/hw_2_2.py
@@ -2,3 +2,4 @@ x = 2
 y = 3
 calculation = x - y / 1 + x * y
 print(f'Ответ: {calculation}')
+
