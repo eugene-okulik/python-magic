7883f730068eb4b5052b926d766d21dbbe0f0d5d
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-08-03 18:18:52 +0300

    fixed. last line added again

--- homework/dima_novolodskiy/Homework_2/hw_2_4.py
+++ homework/dima_novolodskiy/Homework_2/hw_2_4.py
@@ -3,3 +3,4 @@ cat_b = 5
 hypotenuse_triangle = (cat_a ** 2 + cat_b ** 2) ** 0.5
 area_triangle = cat_a * cat_b / 2
 print(f'Гипотинуза треугольника: {hypotenuse_triangle} Площадь треуголника: {area_triangle}')
+
