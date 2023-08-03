bd3b2457d4d078407e120e4ad7d2aaabf9612251
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-08-03 18:06:32 +0300

    home_worke_2 done

---
+++ homework/dima_novolodskiy/Homework_2/hw_2_1.py
@@ -0,0 +1,6 @@
+a = 2
+b = 3
+sum_ab = a + b
+raz_ab = a - b
+pro_ab = a * b
+print(f'Сумма: {sum_ab} Разность: {raz_ab} Произведение: {pro_ab}')
---
+++ homework/dima_novolodskiy/Homework_2/hw_2_4.py
@@ -0,0 +1,5 @@
+cat_a = 4
+cat_b = 5
+hypotenuse_triangle = (cat_a ** 2 + cat_b ** 2) ** 0.5
+area_triangle = cat_a * cat_b / 2
+print(f'Гипотинуза треугольника: {hypotenuse_triangle} Площадь треуголника: {area_triangle}')
