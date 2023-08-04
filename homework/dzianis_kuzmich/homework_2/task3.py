fdedc8dab9f66b62df2647c77a4eb3fb416eab7b
Dzianis Kuzmich <dzianis.kuzmich@gmail.com>
2023-08-04 12:41:54 -0700

    dk_homework_2

---
+++ homework/dzianis_kuzmich/homework_2/task3.py
@@ -0,0 +1,14 @@
+a = 10
+b = 20
+
+average_arithmetic = (a + b) / 2
+
+print(average_arithmetic)
+
+a = 10
+b = 20
+
+average_geometric = (a * b) ** 0.5
+
+print(average_geometric)
+
---
+++ homework/dzianis_kuzmich/homework_2/task4.py
@@ -0,0 +1,9 @@
+side1 = 5
+side2 = 10
+
+hypotenuse = (side1 ** 2 + side2 ** 2) ** 0.5
+area = 0.5 * side1 * side2
+
+print("Hypotenuse:", hypotenuse)
+print("Area:", area)
+
