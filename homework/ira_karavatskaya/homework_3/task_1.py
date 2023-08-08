26546b8e104695854582dc14443211c37ac6aca9
karavatskaya_ira <Karavatskaya_ira@tut.by>
2023-08-08 14:15:36 +0300

    добавила две части задания 3

--- homework/ira_karavatskaya/homework_3/task_1.py
+++ homework/ira_karavatskaya/homework_3/task_1.py
@@ -1,4 +1,46 @@
-print(2)
+my_dict = {'tuple': (2, 5, 8, None, 'text', True), 'list': [5, 6, False, 4.35, None], 'dict': {'key1': 'one',
+                                                                                               'key2': '3',
+                                                                                               'key3': 'True',
+                                                                                               'key4': '5',
+                                                                                               'key5': 'two'},
+           'set': {5, 8, 6, True, None}}
+
+print('Full dictionary:', my_dict)
+
+# выводим последний элемент кортежа my tuple
+
+
+print(my_dict['tuple'][-1])
+
+# добавляем в конец списка 'list' элемент None
+
+my_dict['list'].append(None)
+print('add element "None" to the end of the list:', my_dict['list'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
