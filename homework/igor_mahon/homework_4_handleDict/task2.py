471a4ab0568504c8b63f3d4772e97c2a6adb2998
IgorMakhon <igor.mahon.blr@gmail.com>
2023-08-13 10:15:08 +0300

    homework_4_handleDict_done

---
+++ homework/igor_mahon/homework_4_handleDict/task2.py
@@ -0,0 +1,18 @@
+# Задание 2
+# Допустим, какая-то программа возвращает результат своей работы в таком виде:
+# результат операции: 42
+# результат операции: 54
+# С помощью срезов и метода index получите из каждой строки с результатом число, прибавьте к полученному числу 10,
+# результат сложения распечатайте.
+
+result_one = "результат операции: 42"
+result_two = "результат операции: 54"
+
+phrase = "операции: "
+len_phrase = len(phrase)
+
+details_result_index1 = result_one.lower().index("операции: ") + len_phrase
+details_result_index2 = result_two.lower().index("операции: ") + len_phrase
+
+print(result_one[details_result_index1:])
+print(result_two[details_result_index2:])
