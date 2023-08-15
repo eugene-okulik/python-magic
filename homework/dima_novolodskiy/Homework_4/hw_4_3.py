6724443e1793f30e45a997457f16f0f40634f5a8
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-08-15 17:34:15 +0300

    Homework_4 done

---
+++ homework/dima_novolodskiy/Homework_4/hw_4_3.py
@@ -0,0 +1,5 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+students = ', '.join(students)
+subjects = ['math', 'biology', 'geography']
+subjects = ', '.join(subjects)
+print(f'Students {students} study these subjects: {subjects}')
