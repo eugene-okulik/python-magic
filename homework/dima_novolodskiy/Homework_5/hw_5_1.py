3efb5af13f2417e06b2d7b1755ebaa4c9f0defcb
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-08-20 20:10:15 +0300

    Homework_5 fixe bug

--- homework/dima_novolodskiy/Homework_5/hw_5_1.py
+++ homework/dima_novolodskiy/Homework_5/hw_5_1.py
@@ -1,8 +1,10 @@
-start_str = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+start_str = (
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. '
+    'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+)
 spec_sim = ".,:;!_*-+()/#¤%&) "
 list_start_str = list(start_str)
 list_new_str = []
-d = len(list_start_str)
 for i in range(len(list_start_str)):
     list_new_str.append(list_start_str[i])
     if i != len(list_start_str) - 1:
