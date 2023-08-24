e73f5b78f72333946b11274c9259b2357579a065
irinaelfimova <iren.elfimova@gmail.com>
2023-08-24 08:28:15 -0500

    Irina Elf. Homework lesson 7

--- homework/irina_elfimova/homework_7/hmw_7_1.py
+++ homework/irina_elfimova/homework_7/hmw_7_1.py
@@ -5,7 +5,7 @@ def your_salary():
     salary = int(input("Enter your current salary "))
     bonus = random.choice([True, False])
 
-    if bonus == True:
+    if bonus is True:
         full_salary = salary + random.randint(50, 10000)
         print("Your full salary is ", full_salary)
 
