691ec3316648acb56bb4f63e4c7d57f223536d8e
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-08-28 20:50:26 +0300

    novolodskiy homework_6 upgreat

--- homework/dima_novolodskiy/Homework_6/hw_6_1.py
+++ homework/dima_novolodskiy/Homework_6/hw_6_1.py
@@ -1,13 +1,13 @@
 fis_param, sec_param = list(map(int, input().split()))
 
 
-def pr_function(fist, second):
-    rez = fist * second
-    if rez > 20:
-        rez *= 2
-    else:
-        rez *= 3
-    return rez
+def mply(a, b):
+    return a * b
 
 
-print(pr_function(fis_param, sec_param))
+result = mply(fis_param, sec_param)
+
+if result > 20:
+    print(mply(result, 2))
+else:
+    print(mply(result, 3))
--- homework/dima_novolodskiy/Homework_6/hw_6_2.py
+++ homework/dima_novolodskiy/Homework_6/hw_6_2.py
@@ -1,9 +1,4 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
-
-def dict_word(dict_in: object):
-    for key, values in dict_in.items():
-        print(key * values)
-
-
-dict_word(words)
+for key, values in words.items():
+    print(key * values)
