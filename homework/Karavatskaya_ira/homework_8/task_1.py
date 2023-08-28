56092d634093c6af3fbca685e697d72d7305d448
Karavatskaya Ira <Karavatskaya_ira@tut.by>
2023-08-28 14:53:44 +0300

    Домашнее задание 8 выполнено

---
+++ homework/Karavatskaya_ira/homework_8/task_1.py
@@ -0,0 +1,16 @@
+# Дана такая дата: "Jan 15, 2023 - 12:05:33"
+# Преобразуйте эту дату в питоновский формат, после этого распечатайте полное название месяца из этой даты
+# Распечатайте дату в таком формате: "15.01.2023, 12:05"
+
+
+import datetime
+
+my_time = 'Jan 15, 2023 - 12:15:33'
+
+python_date = datetime.datetime.strptime(my_time, '%b %d, %Y - %H:%M:%S')
+
+print(my_time)
+
+print(python_date.strftime('%B'))
+
+print(python_date.strftime('%d.%m.%Y, %H:%M'))
