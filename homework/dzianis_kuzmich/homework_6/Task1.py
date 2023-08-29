a3eee75d57c74fe8c8bf45b4f657625cbaff0fd1
Dzianis Kuzmich <dzianis.kuzmich@gmail.com>
2023-08-29 14:17:43 -0700

    dk_homework_6_code_review_follow-up

--- homework/dzianis_kuzmich/homework_6/Task1.py
+++ homework/dzianis_kuzmich/homework_6/Task1.py
@@ -1,15 +1,19 @@
+# Функция для умножения двух чисел
+def multiply(a, b):
+    return a * b
+
+
 # Запрос на ввод двух чисел от пользователя
-number1 = float(input("Введите первое число: "))
-number2 = float(input("Введите второе число: "))
+number_one = float(input("Введите первое число: "))
+number_two = float(input("Введите второе число: "))
 
 # Умножение чисел
-result = number1 * number2
+result = multiply(number_one, number_two)
 
 # Проверка условий и дополнительные действия
 if result > 20:
-    result *= 2
+    result = multiply(result, 2)
 else:
-    result *= 3
+    result = multiply(result, 3)
 
-# Вывод результата
 print("Результат: ", result)
