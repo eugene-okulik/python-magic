b9859e96f98e2c83ae2be875db3026ca00d2612f
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-08-29 18:23:41 +0300

    novolodskiy homework_7 upgreat

--- homework/dima_novolodskiy/Homework_7/hw_7_2.py
+++ homework/dima_novolodskiy/Homework_7/hw_7_2.py
@@ -1,15 +1,12 @@
-import sys
-
-sys.set_int_max_str_digits(100000)
-
-
 def fibonachi_func(number):
-    list_number_fibonachi = [0, 1]
-    # cycle_infinite = True
-    while len(list_number_fibonachi) < number:
-        list_number_fibonachi.append(list_number_fibonachi[-2] + list_number_fibonachi[-1])
-        if len(list_number_fibonachi) == number:
-            yield list_number_fibonachi[number - 1]
+    a = 0
+    b = 1
+    count = 2
+    while count != number:
+        a, b = b, a + b
+        count += 1
+        if count == number:
+            yield b
 
 
 list_index_number = [5, 200, 1000, 100000]
