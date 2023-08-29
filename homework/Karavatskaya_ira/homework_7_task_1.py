d9db3d503d1778fa95ef0d2a6c2a68ce131667f1
Karavatskaya Ira <Karavatskaya_ira@tut.by>
2023-08-29 17:42:56 +0300

    Исправлена задача 1 в 7-ом задании

--- homework/Karavatskaya_ira/homework_7_task_1.py
+++ homework/Karavatskaya_ira/homework_7_task_1.py
@@ -9,15 +9,14 @@ import random
 
 def your_salary():
     salary = int(input('Введите значение зарплаты'))
-    bonus = bool(random.getrandbits(1))
-    print(bonus)
+    bonus = random.choice([True, False])
+
     if bonus:
         bonus_value = random.randint(1, 1000)
         nuw_salary = salary + bonus_value
         print(salary, ',', bonus, '-', '`', '$', nuw_salary, '`')
     else:
-        salary = salary
         print(salary, ',', bonus, '-', '`', '$' + str(salary) + '`')
 
 
-print(your_salary())
+your_salary()
