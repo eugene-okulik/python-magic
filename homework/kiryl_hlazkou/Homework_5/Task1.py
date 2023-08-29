ce356e0ba49a083a5e0372ce2a213083105cbb11
Kiryl Hlazkou <kirylhlazkou@gmail.com>
2023-08-29 18:15:16 +0300

    KH: Homework 5 fixed string

--- homework/kiryl_hlazkou/Homework_5/Task1.py
+++ homework/kiryl_hlazkou/Homework_5/Task1.py
@@ -1,5 +1,7 @@
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper' \
-       'at,dignissim vitae libero.'
+text = (
+    'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper'
+    + 'at,dignissim vitae libero.'
+)
 
 new_text = []
 
@@ -8,8 +10,13 @@ words = text.split()
 for word in words:
     if word[-1] in ('.', ','):
         word = word.replace(word[-1], 'ing' + word[-1])
-        # new_text.append(word)
+        new_text.append(word)
     else:
         word = word + 'ing'
         new_text.append(word)
 print(' '.join(new_text))
+
+# Задание №1
+# Напишите программу, которая добавляет ‘ing’ к словам (к каждому слову) в тексте “Etiam tincidunt neque erat, quis
+# molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero” и после этого
+# выводит получившийся текст на экран.
