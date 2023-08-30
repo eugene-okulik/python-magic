f0ecd016119098b56d76dc4292e09a50ac3d0f5f
irinaelfimova <iren.elfimova@gmail.com>
2023-08-30 09:20:55 -0500

    Irina Elf. Homework lesson 6. Updated

--- homework/irina_elfimova/homework 6/homework_6_1.py
+++ homework/irina_elfimova/homework 6/homework_6_1.py
@@ -5,7 +5,12 @@ num2 = int(input())
 
 
 def multiply(n1, n2):
-    n1 * n2
+    return n1 * n2
 
 
-print(multiply(num1, num2))
+num3 = multiply(num1, num2)
+
+if num3 > 20:
+    print(num3 * 2)
+else:
+    print(num3 * 3)
