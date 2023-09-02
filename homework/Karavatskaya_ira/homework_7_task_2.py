4fd603df427ea127f6d13d48e21a2e2daca85e18
Karavatskaya Ira <Karavatskaya_ira@tut.by>
2023-09-02 11:26:49 +0300

    Исправлены ошибки в домашнем задании 7

--- homework/Karavatskaya_ira/homework_7_task_2.py
+++ homework/Karavatskaya_ira/homework_7_task_2.py
@@ -1,17 +1,21 @@
 # Напишите функцию-генератор, которая генерирует список чисел фибоначчи.
 # Распечатайте из этого списка пятое, двухсотое, тысячное и стотысячное число.
 
-def fibonacci_list(f_num):
-    f1 = 1
-    f2 = 1
-    for i in range(f_num):
-        yield f1
-        f1, f2 = f2, f1 + f2
+
+def fibonacci_generator():
+    a, b = 0, 1
+
+    while True:
+        yield a
+        a, b = b, a + b
 
 
-fib_list = list(fibonacci_list(100000))
+fibonacci_numbers = []
+generator = fibonacci_generator()
+for i in range(100001):
+    fibonacci_numbers.append(next(generator))
 
-print(fib_list[4])
-print(fib_list[199])
-print(fib_list[999])
-print(fib_list[99999])
+print(fibonacci_numbers[4])
+print(fibonacci_numbers[199])
+print(fibonacci_numbers[999])
+print(fibonacci_numbers[99999])
