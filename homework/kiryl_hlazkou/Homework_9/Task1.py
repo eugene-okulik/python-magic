133a02c93e5fca7e3bdb5fce63b3afe5acf2886d
Kiryl Hlazkou <kirylhlazkou@gmail.com>
2023-09-06 18:32:09 +0300

    KH: Homework 9 (decorators and list comprehension) done

---
+++ homework/kiryl_hlazkou/Homework_9/Task1.py
@@ -0,0 +1,50 @@
+def operation_decorator(func):
+    def wrapper(first, second):
+        if first == second:
+            return func(first, second, '+')
+        elif first > second:
+            return func(first, second, '-')
+        elif second > first:
+            return func(first, second, '/')
+        if first < 0 or second < 0:
+            return func(first, second, '*')
+    return wrapper
+
+
+@operation_decorator
+def calc(first, second, operation):
+    if operation == '+':
+        return first + second
+    elif operation == '-':
+        return first - second
+    elif operation == '/':
+        if second == 0:
+            return 'Cannot divide by zero'
+        return first / second
+    elif operation == '*':
+        return first * second
+    else:
+        return 'Invalid operation'
+
+
+first_number = int(input('Enter first number: '))
+second_number = int(input('Enter first number: '))
+
+result = calc(first_number, second_number)
+
+print('Result:', result)
+
+# Напишите программу: Есть функция которая делает одну из арифметических операций с переданными ей числами
+# (числа и операция передаются в аргументы функции). Функция выглядит примерно так:
+#
+# def calc(first, second, operation):
+#     if opertaion == '+':
+#     .....
+# Программа спрашивает у пользователя 2 числа (вне функции)
+#
+# Создайте декоратор, который декорирует функцию calc и управляет тем какая операция будет произведена:
+#
+# если числа равны, то функция calc вызывается с операцией сложения этих чисел
+# если первое больше второго, то происходит вычитание второго из певрого
+# если второе больше первого - деление первого на второе
+# если одно из чисел отрицательное - умножение
