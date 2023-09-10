0ee76d4ae4876eb638e8b59bf1b3a22547dba1a0
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-09-10 16:32:31 +0200

    novolodskiy home work 11 fixe 2.0

--- homework/dima_novolodskiy/Homework_11/hw_11.py
+++ homework/dima_novolodskiy/Homework_11/hw_11.py
@@ -22,22 +22,27 @@ class Tulips(Flower):
 class Bouquet:
 
     def __init__(self, *args: object):
-        self.args = args
+        self.args = list(args)
+        self.price = self.total_price()
+        self.freshness = self.total_freshness()
+
+    def total_price(self):
+        total = []
+        for price_unit in self.args:
+            total.append(price_unit.price)
+        return sum(total)
+
+    def total_freshness(self):
+        total_fresh = []
+        for fresh_unit in self.args:
+            total_fresh.append(fresh_unit.freshness)
+        return sum(total_fresh) / len(total_fresh)
 
 
-    def list_flowers(self):
-        lst = list(self.args)
-        return lst
-
-
-'''1. Создать классы цветов: общий класс для всех цветов и классы для нескольких видов. 
-2. Создать экземпляры (объекты) цветов разных видов.
-3. Собрать букет (букет - еще один класс) с определением его стоимости. 
-    В букете цветы пусть хранятся в списке. 
-    Это будет список объектов.
-4. Для букета создать метод, который определяет время его увядания по среднему времени жизни всех цветов в букете.
+'''
 5. Позволить сортировку цветов в букете на основе различных параметров (свежесть/цвет/длина стебля/стоимость)(это тоже методы)
-6. Реализовать поиск цветов в букете по каким-нибудь параметрам (например, по среднему времени жизни) (и это тоже метод).'''
+6. Реализовать поиск цветов в букете по каким-нибудь параметрам (например, по среднему времени жизни) (и это тоже метод).
+'''
 
 rose_1 = Rose('red', 50, 120, 5)
 rose_2 = Rose('yello', 40, 110, 4)
@@ -45,10 +50,11 @@ rose_3 = Rose('white', 30, 100, 3)
 peonies_1 = Peonies('white', 40, 250, 5)
 peonies_2 = Peonies('lilac', 40, 320, 5)
 peonies_3 = Peonies('pink', 40, 220, 5)
-tulips_1 = Tulips('red', 50, 90, 2)
-tulips_2 = Tulips('red', 50, 90, 2)
-tulips_3 = Tulips('red', 50, 90, 2)
+tulips_1 = Tulips('red', 50, 90, 3)
+tulips_2 = Tulips('red', 50, 90, 3)
+tulips_3 = Tulips('red', 50, 90, 3)
 
 bouquet = Bouquet(rose_1, rose_2, rose_3, peonies_1, peonies_2, peonies_3, tulips_1, tulips_2, tulips_3)
 
-print(bouquet.list_flowers()[0].color)
+print(bouquet.price)
+print(bouquet.freshness)
