70443ccd784c81d13c2864fe5af2843c6da62e47
Dmitriy Novolodskiy <novolodskiyda@gmail.com>
2023-09-12 17:50:43 +0200

    novolodskiy home work 11 update

--- homework/dima_novolodskiy/Homework_11/hw_11.py
+++ homework/dima_novolodskiy/Homework_11/hw_11.py
@@ -23,56 +23,43 @@ class Tulips(Flower):
 class Bouquet:
 
     def __init__(self, *args: object):
-        self.args = list(args)
-        self.price = self.total_price()
-        self.freshness = self.total_freshness()
+        self.args = args
+        self.price_b = self.price_b()
+        self.freshness_b = self.freshness_b()
+        self.sorted_price = self.sorted_price_flower()
+        self.sorted_color = self.sorted_color_flower
+        self.sorted_length = self.sorted_length_flower
+        self.sorted_freshness = self.sorted_freshness_flower
 
-    def total_price(self):
-        return sum([price_unit.price for price_unit in self.args])
+    def price_b(self):
+        total = sum([price_unit.price for price_unit in self.args])
+        return total
 
-    def total_freshness(self):
+    def freshness_b(self):
         total_fresh = []
         for fresh_unit in self.args:
             total_fresh.append(fresh_unit.freshness)
-        return sum(total_fresh) / len(total_fresh)
+        return f'{sum(total_fresh) / len(total_fresh)}'
 
-    @property
-    def sorted_price(self):
-        set_price = [{'name': price_u.name, 'price': price_u.price} for price_u in self.args]
+    def sorted_price_flower(self):
+        price_dict = {price_u.name: price_u.price for price_u in self.args}
+        sorted_p = dict(sorted(price_dict.items(), key=lambda item: item[1]))
+        return [flower for flower in sorted_p]
 
-        def get_name(dictionary):
-            return dictionary['price']
+    def sorted_color_flower(self):
+        color_dict = {color_u.name: color_u.color for color_u in self.args}
+        sorted_color = dict(sorted(color_dict.items(), key=lambda item: item[1]))
+        return [flower for flower in sorted_color]
 
-        return set_price.sort(key=get_name(set_price))
+    def sorted_length_flower(self):
+        length_dict = {length_u.name: length_u.length for length_u in self.args}
+        sorted_length = dict(sorted(length_dict.items(), key=lambda item: item[1]))
+        return [flower for flower in sorted_length]
 
-    def sorted_color(self):
-        set_color = {color_u.name: color_u.color for color_u in self.args}
-        sorted_name = []
-        sorted_color = ['red']
-        for key, value in set_color.items():
-            if value >= max(sorted_color):
-                sorted_name.append(key)
-                sorted_color.append(value)
-            else:
-                sorted_name = [key] + sorted_name
-                sorted_color.append(value)
-        return sorted_name
-
-    # def sorted_length(self):
-    #     set_length = {length_u.name: length_u.length for length_u in self.args}
-    #     sorted_name_length = []
-    #     sorted_length = ['red']
-    #     for key, value in set_length.items():
-    #         if value >= max(sorted_length):
-    #             sorted_name_length.append(key)
-    #             sorted_length.append(value)
-    #         else:
-    #             sorted_name_length = [key] + sort
-    #             sorted_length.append(value)
-    #     return sort
-
-    def sorted_freshness(self):
-        return sorted([freshness_u.freshness for freshness_u in self.args])
+    def sorted_freshness_flower(self):
+        freshness_dict = {freshness_u.name: freshness_u.freshness for freshness_u in self.args}
+        sorted_freshness = dict(sorted(freshness_dict.items(), key=lambda item: item[1]))
+        return [flower for flower in sorted_freshness]
 
     def flower_search(self, param):
         return [f'{flower_name.name} {flower_name.color}' for flower_name in self.args if
@@ -91,10 +78,8 @@ tulips_3 = Tulips('red', 50, 90, 3, 'Красный тюльпан 3')
 
 bouquet_1 = Bouquet(rose_1, rose_2, rose_3, peonies_1, peonies_2, peonies_3, tulips_1, tulips_2, tulips_3)
 
-# print(bouquet_1.total_price())
-# print(bouquet_1.total_freshness())
 print(bouquet_1.sorted_price)
-# print(bouquet_1.sorted_color())
-# print(bouquet_1.sorted_length())
-# print(bouquet_1.sorted_freshness())
-# print(bouquet_1.flower_search(5))
+print(bouquet_1.sorted_color())
+print(bouquet_1.sorted_length())
+print(bouquet_1.sorted_freshness())
+print(bouquet_1.flower_search(5))
