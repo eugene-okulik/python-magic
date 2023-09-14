39b4bf8e45f3e990cee00ef160e996de1ebe96fd
irinaelfimova <iren.elfimova@gmail.com>
2023-09-13 19:23:45 -0500

    Irina Elf. Homework lesson 7. UPDATED

--- homework/irina_elfimova/homework_7/hmw_7_2.py
+++ homework/irina_elfimova/homework_7/hmw_7_2.py
@@ -1,18 +1,21 @@
 import sys
-def febonachu_gen(f_num):
+
+
+def febonachu_gen():
     a, b = 0, 1
     while True:
         yield a
         a, b = b, a + b
 
-def fibonacci_number(n):
+
+def febonachu_number(n):
     fib_gen = febonachu_gen()
     for _ in range(n):
         fib_number = next(fib_gen)
     return fib_number
 
-print(fibonacci_number(5))
-print(fibonacci_number(200))
-print(fibonacci_number(1000))
-print(fibonacci_number(100000))
 
+print(febonachu_number(5))
+print(febonachu_number(200))
+print(febonachu_number(1000))
+print(febonachu_number(100000))
