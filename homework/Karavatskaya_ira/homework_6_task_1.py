34da40f5f8b1a1e1e9d2f3d7a3dbfe1ae956e798
Karavatskaya Ira <Karavatskaya_ira@tut.by>
2023-09-15 15:53:25 +0300

    Исправила функцию def в ДЗ 6

--- homework/Karavatskaya_ira/homework_6_task_1.py
+++ homework/Karavatskaya_ira/homework_6_task_1.py
@@ -5,18 +5,19 @@
 # Если результат изначального умножения меньше или равно 20, то он умножается на 3.
 
 
-def multiply_numbers():
-    numb_1 = int(input('Введите первое число: '))
-    numb_2 = int(input('Введите второе число: '))
+def multiply_numbers(numb_1, numb_2):
+    return numb_1 * numb_2
 
-    resalt = numb_1 * numb_2
 
-    if resalt > 20:
-        resalt *= 2
+def resalt(numb_3, numb_4):
+    if multiply_numbers(numb_3, numb_4) > 20:
+        return print(multiply_numbers(numb_3, numb_4) * 2)
     else:
-        resalt *= 3
-
-    print('Результат:', resalt)
+        return print(multiply_numbers(numb_3, numb_4) * 3)
 
 
-multiply_numbers()
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+
+
+resalt(a, b)
