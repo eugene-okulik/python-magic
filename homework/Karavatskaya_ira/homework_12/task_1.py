923a6cff587e3fea3796e6395f99a5a54629e39f
Karavatskaya Ira <Karavatskaya_ira@tut.by>
2023-10-01 15:06:52 +0300

    Домашнее задание 12 выполнено

--- homework/Karavatskaya_ira/homework_12/task_1.py
+++ homework/Karavatskaya_ira/homework_12/task_1.py
@@ -1,40 +1,36 @@
-import datetime
 import os
-from datetime import date
-
+from datetime import datetime, timedelta
+import calendar
 
 main_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 file_path = os.path.join(main_path, 'eugene_okulik', 'hw_12', 'data.txt')
 
 with open(file_path, 'r') as new_file:
-    new_date_1 = new_file.readline()
-    new_date_2 = new_file.readline()
-    new_date_3 = new_file.readline()
+    data = new_file.read()
+    print(data)
 
+lines = data.split('\n')
 
-def new_data(data, time):
-    dat = data.split('-')
-    dat[0] = str(int(dat[0]) + 3)
-    result = '-'.join(dat) + ' ' + time
-    return result
+date_parts_1 = lines[0].split(' ')[1].split(' - ')
+original_date_1 = ' '.join(date_parts_1)
+data_1 = datetime.strptime(original_date_1, '%Y-%m-%d')
+# Добавляем три года к дате
+three_years_later = data_1 + timedelta(days=1096)
+print(three_years_later)
 
+date_parts_2 = lines[1].split(' ')[1].split(' - ')
+original_date_2 = ' '.join(date_parts_2)
+data_2 = datetime.strptime(original_date_1, '%Y-%m-%d')
+weekday = data_2.weekday()
+# Получаем название дня недели
+weekday_name = calendar.day_name[weekday]
+print(weekday_name)
 
-def week_day(data):
-    new_d = data.split('-')
-    year = int(new_d[0])
-    month = int(new_d[1])
-    day = int(new_d[2])
-    w_day = (datetime.datetime(year, month, day))
-    return w_day.strftime('%A')
-
-
-def day_before(data):
-    today = date.today()
-    before = date.fromisoformat(data)
-    delta = abs(today - before)
-    return delta.days
-
-
-print('На три года позже:', new_data)
-print('День недели:', week_day)
-print('Cколько дней назад была эта дата:', day_before)
+date_parts_3 = lines[2].split('. ')[1].split(' -')[0]
+print(date_parts_3)
+date_3 = datetime.strptime(date_parts_3, '%Y-%m-%d %H:%M:%S.%f')
+print(date_3)
+current_date = datetime.now()
+# Вычисляем разницу между текущей датой и указанной датой
+days_ago = (current_date - date_3).days
+print(days_ago)
