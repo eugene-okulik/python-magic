052ed544b18fdd18ac5cf2e25bf9ecdc7c782df9
Kiryl Hlazkou <kirylhlazkou@gmail.com>
2023-10-04 19:01:48 +0300

    KH: Homework 7 fixed

--- homework/kiryl_hlazkou/Homework_7/Task2.py
+++ homework/kiryl_hlazkou/Homework_7/Task2.py
@@ -1,16 +1,20 @@
 # Напишите функцию-генератор, которая генерирует список чисел фибоначчи
 # Распечатайте из этого списка пятое число, двухсотое число, тысячное число, стотысячное число
-def fib(n):
+def fib():
     a, b = 0, 1
-    for x in range(n):
+    while True:
         yield a
         a, b = b, a + b
 
 
-list_fib = (list(fib(1001)))
+def fib_num(n):
+    fib_gen = fib()
+    fib_number = 0  # Initialize fib_number before the loop
+    for _ in range(n):
+        fib_number = next(fib_gen)
+    return fib_number
 
-print(list_fib[5])
 
-print(list_fib[200])
-
-print(list_fib[1000])
+print(fib_num(5))
+print(fib_num(200))
+print(fib_num(1000))
