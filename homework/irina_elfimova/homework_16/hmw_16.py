573826e7b76b469529dc1d063432c6d431730287
irinaelfimova <iren.elfimova@gmail.com>
2023-10-05 14:28:46 -0500

    Irina Elf. Homework 16

--- homework/irina_elfimova/homework_16/hmw_16.py
+++ homework/irina_elfimova/homework_16/hmw_16.py
@@ -3,20 +3,17 @@ import json
 
 
 def new_link():
-    payload = json.dumps({
+    body = json.dumps({
         "input": "https://amazon.com/very-long-url"
     })
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
     first_response = requests.post(
         "https://gotiny.cc/api",
         headers=headers,
-        data=payload
+        data=body
     ).json()
 
     print(first_response)
-    assert first_response['long'] == "https://amazon.com/very-long-url"
 
 
 def new_cus_link():
@@ -25,15 +22,14 @@ def new_cus_link():
         "long": "https://google.com/very-long-url",
         "custom": "google"
     })
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
     second_response = requests.post(
         url,
         headers=headers,
         data=payload
     ).json()
     print(second_response)
+
 
 def new_full_link():
     url = "https://gotiny.cc/api"
@@ -42,10 +38,34 @@ def new_full_link():
         "custom": "amazon",
         "useFallback": True
     })
-    headers = {
-        'Content-Type': 'application/json'
-    }
+    headers = {'Content-Type': 'application/json'}
     response = requests.request("POST", url, headers=headers, data=payload)
     print(response.text)
 
+
+def link_as_text():
+    url = "https://gotiny.cc/api/br7a3x"
+    payload = {}
+    headers = {}
+    response = requests.get(url, headers=headers, data=payload)
+    print(response.text)
+
+
+def link_as_json():
+    url = "https://gotiny.cc/api/nmytrk"
+    payload = {}
+    headers = {}
+    response = requests.get(
+        url,
+        headers=headers,
+        data=payload
+    )
+    new_data = json.loads(response)
+    print(new_data)
+
+
 print(new_link())
+print(new_cus_link())
+print(new_full_link())
+print(link_as_text())
+print(link_as_json())
