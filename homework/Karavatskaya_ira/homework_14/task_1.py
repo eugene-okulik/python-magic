5bd494a998323527418b5d9e6257cab976add80d
Karavatskaya Ira <Karavatskaya_ira@tut.by>
2023-10-06 15:14:51 +0300

    Добавлено cursor.lastrowid

--- homework/Karavatskaya_ira/homework_14/task_1.py
+++ homework/Karavatskaya_ira/homework_14/task_1.py
@@ -10,17 +10,19 @@ db = mysql.connect(
 
 
 cursor = db.cursor(dictionary=True)
+
 cursor.execute("INSERT INTO `groups` (title, start_date,end_date) values ('AQA-01', 'jun-2023', 'des-2024')")
 db.commit()
 cursor.execute("INSERT INTO students (name, second_name, group_id) values ('Martin', 'Lutter', 11)")
+last_id = cursor.lastrowid
 db.commit()
-cursor.execute("INSERT INTO books (title, taken_by_student_id) values ('Lutherbibel', 17)")
+cursor.execute(f"INSERT INTO books (title, taken_by_student_id) values ('Lutherbibel', {last_id})")
 db.commit()
-cursor.execute("INSERT INTO books (title, taken_by_student_id) values ('Philosophy', 17)")
+cursor.execute("INSERT INTO books (title, taken_by_student_id) values ('Philosophy',  {last_id})")
 db.commit()
-cursor.execute("INSERT INTO books (title, taken_by_student_id) values ('Theology', 17)")
+cursor.execute("INSERT INTO books (title, taken_by_student_id) values ('Theology',  {last_id})")
 db.commit()
-cursor.execute("UPDATE students SET group_id = 11 WHERE id = 17")
+cursor.execute("UPDATE students SET group_id = 11 WHERE id =  {last_id}")
 db.commit()
 cursor.execute("INSERT INTO subjets (title) VALUES ('history')")
 db.commit()
@@ -38,23 +40,23 @@ cursor.execute("INSERT INTO lessons (title, subject_id) VALUES ('classes one', 9
 db.commit()
 cursor.execute("INSERT INTO lessons (title, subject_id) VALUES ('classes two', 9)")
 db.commit()
-cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (8, 12, 17)")
+cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (8, 12,  {last_id})")
 db.commit()
-cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (7, 13, 17)")
+cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (7, 13,  {last_id})")
 db.commit()
-cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (9, 14, 17)")
+cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (9, 14,  {last_id})")
 db.commit()
-cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (6, 15, 17)")
+cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (6, 15,  {last_id})")
 db.commit()
-cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (10, 16, 17)")
+cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (10, 16,  {last_id})")
 db.commit()
-cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (7, 17, 17)")
+cursor.execute("INSERT INTO marks (value, lesson_id, student_id) VALUES (7, 17,  {last_id})")
 db.commit()
 
-cursor.execute("SELECT value FROM marks WHERE student_id = 17")
+cursor.execute("SELECT value FROM marks WHERE student_id =  {last_id}")
 print(cursor.fetchall())
 
-cursor.execute("SELECT title FROM books WHERE taken_by_student_id = 17")
+cursor.execute("SELECT title FROM books WHERE taken_by_student_id =  {last_id}")
 print(cursor.fetchall())
 
 cursor.execute('''
@@ -65,7 +67,7 @@ JOIN books
 ON books.taken_by_student_id = students.id
 JOIN marks
 ON marks.student_id = students.id
-WHERE students.id = 17
+WHERE students.id =  {last_id}
 ''')
 print(cursor.fetchall())
 
