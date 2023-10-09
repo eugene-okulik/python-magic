210450a78c7d7eb9147637029e4ca2dbaf00cdcf
Eugeny Okulik <tiburon854@gmail.com>
2023-10-09 18:15:43 +0300

    Files from lesson 18

---
+++ homework/eugene_okulik/Lesson_18/test_dir1/test_pytest.py
@@ -0,0 +1,62 @@
+import random
+
+import requests
+import pytest
+import time
+
+
+@pytest.fixture(scope='session')
+def hello():
+    print('hello')
+    yield
+    print('Bye')
+
+
+@pytest.mark.smoke
+def test_get_one_post(new_post_id, hello):
+    print('test')
+    response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{new_post_id}').json()
+    assert response == {11}
+
+
+@pytest.mark.smoke
+def test_get_all_posts(hello):
+    print('test')
+    response = requests.get('https://jsonplaceholder.typicode.com/posts').json()
+    assert len(response) == 100
+
+
+@pytest.mark.regression
+def test_add_post(hello):
+    print('test')
+    body = {
+        "title": "fsakjdhfkasjdhflkajsdhlkfjashdfoo",
+        "body": "barasdfaskdjfhlaksdfoiwueysdhgkjashdkfjhalskdjfhasdf",
+        "userId": 1
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(
+        'https://jsonplaceholder.typicode.com/posts',
+        json=body,
+        headers=headers
+    )
+    assert response.status_code == 201
+    assert response.json()['id'] == 101
+
+
+@pytest.mark.regression
+def test_one():
+    # time.sleep(3)
+    assert 1 == 1
+
+
+@pytest.mark.parametrize('logins', [1, 2, 3, 4])
+def test_two(logins):
+    print(logins)
+    # time.sleep(3)
+    assert 1 == 1
+
+
+def test_three():
+    # time.sleep(3)
+    assert 1 == 1
