dfe95363af98c359e7a72981e3b758535bf26cc8
Eugeny Okulik <tiburon854@gmail.com>
2023-10-11 08:44:36 +0300

    Files from lesson 19

--- homework/eugene_okulik/Lesson_18/test_dir1/test_pytest.py
+++ homework/eugene_okulik/Lesson_18/test_dir1/test_pytest.py
@@ -1,8 +1,9 @@
-import random
+# import random
 
 import requests
 import pytest
-import time
+# import time
+import allure
 
 
 @pytest.fixture(scope='session')
@@ -12,13 +13,24 @@ def hello():
     print('Bye')
 
 
+@allure.feature('Posts')
+@allure.story('Get posts')
 @pytest.mark.smoke
 def test_get_one_post(new_post_id, hello):
     print('test')
-    response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{new_post_id}').json()
-    assert response == {11}
+    with allure.step(f'Run get request for post with id {new_post_id}'):
+        response = requests.get(f'https://jsonplaceholder.typicode.com/posts/{new_post_id}').json()
+    with allure.step(f'Check that post id is {new_post_id}'):
+        assert response == {11}
 
 
+@allure.feature('Posts')
+@allure.story('Get posts')
+@allure.title('Получение всех постов')
+@allure.issue(
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/RedCat_8727.jpg/220px-RedCat_8727.jpg',
+    'ST-42'
+)
 @pytest.mark.smoke
 def test_get_all_posts(hello):
     print('test')
@@ -26,30 +38,40 @@ def test_get_all_posts(hello):
     assert len(response) == 100
 
 
+@allure.feature('Posts')
+@allure.story('Manipulate posts')
 @pytest.mark.regression
 def test_add_post(hello):
     print('test')
-    body = {
-        "title": "fsakjdhfkasjdhflkajsdhlkfjashdfoo",
-        "body": "barasdfaskdjfhlaksdfoiwueysdhgkjashdkfjhalskdjfhasdf",
-        "userId": 1
-    }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(
-        'https://jsonplaceholder.typicode.com/posts',
-        json=body,
-        headers=headers
-    )
-    assert response.status_code == 201
-    assert response.json()['id'] == 101
+    with allure.step('Prepare test data'):
+        body = {
+            "title": "fsakjdhfkasjdhflkajsdhlkfjashdfoo",
+            "body": "barasdfaskdjfhlaksdfoiwueysdhgkjashdkfjhalskdjfhasdf",
+            "userId": 1
+        }
+        headers = {'Content-Type': 'application/json'}
+    with allure.step('Run request to create a post'):
+        response = requests.post(
+            'https://jsonplaceholder.typicode.com/posts',
+            json=body,
+            headers=headers
+        )
+    with allure.step('Check response code is 201'):
+        assert response.status_code == 201
+    with allure.step('Check id of create post is 101'):
+        assert response.json()['id'] == 101
 
 
+@allure.feature('Example')
+@allure.story('Equals')
 @pytest.mark.regression
 def test_one():
     # time.sleep(3)
     assert 1 == 1
 
 
+@allure.feature('Example')
+@allure.story('Equals')
 @pytest.mark.parametrize('logins', [1, 2, 3, 4])
 def test_two(logins):
     print(logins)
@@ -57,6 +79,8 @@ def test_two(logins):
     assert 1 == 1
 
 
+@allure.feature('Example')
+@allure.story('Equals')
 def test_three():
     # time.sleep(3)
     assert 1 == 1
--- homework/eugene_okulik/Lesson_18/test_dir2/test_pytest2.py
+++ homework/eugene_okulik/Lesson_18/test_dir2/test_pytest2.py
@@ -1,5 +1,6 @@
 import requests
 import pytest
+import allure
 
 
 @pytest.fixture()
@@ -7,9 +8,13 @@ def num():
     return 3
 
 
+@allure.feature('Posts')
+@allure.story('Manipulate posts')
 def test_delete(new_post_id):
     requests.delete(f'https://jsonplaceholder.typicode.com/posts/{new_post_id}')
 
 
+@allure.feature('Example')
+@allure.story('Print')
 def test_num(num):
     print(num)
