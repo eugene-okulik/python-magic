9aa2e48f252e04805e0ac5e46cf577c0329c1258
Kiryl Hlazkou <kirylhlazkou@gmail.com>
2023-10-12 22:06:39 +0300

    KH: Homework 17 fixed

--- homework/kiryl_hlazkou/Homework_16/test_pytest.py
+++ homework/kiryl_hlazkou/Homework_16/test_pytest.py
@@ -21,40 +21,40 @@ def get_code_and_long_url(code):
 
 @pytest.fixture(scope='session')
 def setup_session():
-    print("Start testing")
+    print('Start testing')
     yield
-    print("Testing completed")
+    print('Testing completed')
 
 
 @pytest.fixture
 def setup_test():
-    print("Before test")
+    print('Before test')
     yield
-    print("After test")
+    print('After test')
 
 
-@pytest.mark.parametrize("input_body, expected_long_url", [
+@pytest.mark.parametrize('input_body, expected_long_url', [
     (
-        {"input": "https://www.gismeteo.by/weather-vitebsk-4218/"},
-        "https://www.gismeteo.by/weather-vitebsk-4218/"
+        {'input': 'https://www.gismeteo.by/weather-vitebsk-4218/'},
+        'https://www.gismeteo.by/weather-vitebsk-4218/'
     ),
     (
-        {"long": "https://www.gismeteo.by/weather-vitebsk-4219/", "custom": "1test-gis"},
-        "https://www.gismeteo.by/weather-vitebsk-4219/"
+        {'long': 'https://www.gismeteo.by/weather-vitebsk-4219/', 'custom': '1test-gis'},
+        'https://www.gismeteo.by/weather-vitebsk-4219/'
     ),
     (
-        {"long": "https://www.gismeteo.by/weather-vitebsk-4220/", "custom": "2test-gis", "userFallback": "false"},
-        "https://www.gismeteo.by/weather-vitebsk-4220/"
+        {'long': 'https://www.gismeteo.by/weather-vitebsk-4220/', 'custom': '2test-gis', 'userFallback': 'false'},
+        'https://www.gismeteo.by/weather-vitebsk-4220/'
     ),
 ])
 @allure.feature('Posts')
 @allure.story('Links')
 @pytest.mark.critical
 def test_post_and_assertions(setup_session, setup_test, input_body, expected_long_url):
-    with allure.step(f'Run post with inputs'):
+    with allure.step('Run post with inputs'):
         response = post(input_body)
 
-    with allure.step(f'Check if the response status code is 200'):
+    with allure.step('Check if the response status code is 200'):
         assert response.status_code == 200
 
     # Process the response JSON
@@ -64,19 +64,19 @@ def test_post_and_assertions(setup_session, setup_test, input_body, expected_lon
         assert len(response_json) >= 1
 
     # Extract the code from the response JSON
-    code = response_json[0]["code"]
+    code = response_json[0]['code']
 
     # Construct the full URL by appending the code to "https://gotiny.cc/"
-    full_url = f"https://gotiny.cc/{code}"
+    full_url = f'https://gotiny.cc/{code}'
 
     # Check if the constructed URL redirects to the expected long URL
     redirect_response = requests.get(full_url, allow_redirects=False)
     with allure.step('302 is the HTTP status code for a temporary redirect'):
         assert redirect_response.status_code == 302
     with allure.step('Check expected long URL in Location header'):
-        assert redirect_response.headers["Location"] == expected_long_url
+        assert redirect_response.headers['Location'] == expected_long_url
 
     # Check GET request for code and long URL
     get_response = get_code_and_long_url(code)
-    assert get_response["code"] == code
-    assert get_response["long"] == expected_long_url
+    assert get_response['code'] == code
+    assert get_response['long'] == expected_long_url
