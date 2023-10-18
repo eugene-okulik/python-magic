87e29328d0a34bcb8ab040b2ad9781dc4a85b9ce
Karavatskaya Ira <Karavatskaya_ira@tut.by>
2023-10-18 18:50:02 +0300

    Домашнее задание 17 выполнено

--- homework/Karavatskaya_ira/homework_17/test_task_1.py
+++ homework/Karavatskaya_ira/homework_17/test_task_1.py
@@ -29,7 +29,7 @@ def test_create_link(long_url):
     })
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, headers=headers, data=payload).json()
-    assert response["status"] == 200
+    assert response.status_code == 200
 
 
 @pytest.mark.critical
