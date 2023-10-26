c99f730078b655e0b59b0a5712130997447e8c3c
irinaelfimova <iren.elfimova@gmail.com>
2023-10-26 09:15:41 -0500

    Iren Elf. HMW 22. Selenium first

---
+++ homework/irina_elfimova/homework_22/hmw_22.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+
+options = Options()
+driver = webdriver.Chrome(options=options)
+driver.get('https://www.google.com/')
+
+
+def test_tittle_page():
+    driver.find_element(By.NAME, 'q').send_keys('cat')
+    driver.find_element(By.ID, 'APjFqb').send_keys(Keys.ENTER)
+    get_title = driver.title
+    assert get_title == 'cat - Поиск в Google'
+    print(get_title)
