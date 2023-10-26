f2c8cb13301f7f691251bcfca0c0ffe8336d9b76
Dzianis Kuzmich <dzianis.kuzmich@gmail.com>
2023-10-26 15:19:09 -0400

    dk_homework_22_done

---
+++ homework/dzianis_kuzmich/homework_22/homework_22.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+options = Options()
+driver = webdriver.Chrome(options=options)
+driver.maximize_window()
+driver.get('https://www.google.com/')
+print(driver.title)
+print(driver.current_url)
+search_input = driver.find_element(By.NAME, 'q')
+search_input.send_keys('cat')
+search_input.send_keys(Keys.RETURN)
+
+sleep(4)
+
+if driver.title.startswith("cat"):
+    print("Title starts with 'cat'!")
+else:
+    print(f"Title does not start with 'cat'. Title is: {driver.title}")
+
+
+driver.quit()
