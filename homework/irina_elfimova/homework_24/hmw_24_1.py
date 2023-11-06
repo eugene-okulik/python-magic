c5665b778418f514c9df4e69e7d7c11914abe682
irinaelfimova <iren.elfimova@gmail.com>
2023-11-06 12:06:48 -0600

    Iren Elf. HMW 24_1 and 24_2

--- homework/irina_elfimova/homework_24/hmw_24_1.py
+++ homework/irina_elfimova/homework_24/hmw_24_1.py
@@ -9,9 +9,11 @@ driver = webdriver.Chrome(options=options)
 def test_untitled():
     driver.get("https://www.qa-practice.com/elements/select/single_select")
     driver.set_window_size(1248, 1285)
-    driver.find_element(By.ID, "id_choose_language").click()
+    language = driver.find_element(By.ID, "id_choose_language")
+    language.click()
     dropdown = driver.find_element(By.ID, "id_choose_language")
     dropdown.find_element(By.XPATH, "//option[. = 'JavaScript']").click()
-    driver.find_element(By.ID, "submit-id-submit").click()
+    submit = driver.find_element(By.ID, "submit-id-submit")
+    submit.click()
     selected_lg = driver.find_element(By.ID, "result-text").text
     assert selected_lg == "JavaScript"
--- homework/irina_elfimova/homework_24/hmw_24_2.py
+++ homework/irina_elfimova/homework_24/hmw_24_2.py
@@ -10,7 +10,8 @@ driver.implicitly_wait(6)
 def test_untitled():
     driver.get("https://the-internet.herokuapp.com/dynamic_loading/2")
     driver.set_window_size(1248, 1285)
-    driver.find_element(By.CSS_SELECTOR, "button").click()
+    start_button = driver.find_element(By.CSS_SELECTOR, "button")
+    start_button.click()
     msg_text = driver.find_element(By.XPATH, "//h4[normalize-space()='Hello World!']").text
     assert msg_text == "Hello World!"
 
