5eb588769efdbbcb1702f8c2ef3d47ee4749374a
Kiryl Hlazkou <kirylhlazkou@gmail.com>
2023-11-14 19:30:53 +0300

    KH: Homework 24 fixed

--- homework/kiryl_hlazkou/Homework_24/Task1.py
+++ homework/kiryl_hlazkou/Homework_24/Task1.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
 driver.get("https://www.qa-practice.com/elements/select/single_select")
 
 
 def test_submit():
-    driver.find_element(By.ID, "id_choose_language").click()
-    driver.find_element(By.XPATH, "//option[contains(text(),'Python')]").click()
+    dropdown = driver.find_element(By.ID, "id_choose_language")
+    select = Select(dropdown)
+    select.select_by_visible_text("Python")
 
     driver.find_element(By.ID, "submit-id-submit").click()
 
